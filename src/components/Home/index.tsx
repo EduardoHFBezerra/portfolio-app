6ce4e79d906a5d2223669de7c1a54eb3f2c83af8
EduardoHFBezerra <eduardohfbezerra@gmail.com>
2022-12-07 18:03:13 -0300

    Alteração do pacote de texto digitado

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,26 @@
 import { Section, FadeIn } from "./styles";
-import Typed from "react-typed";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 
 export const Home = () => {
+  // Create Ref element.
+  const el = useRef(null!);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["I'm a Full Stack Developer", "I Love Software Development", "I Love to code"],
+      startDelay: 500,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 200,
+      loop: true
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <Section
       id="home"
@@ -10,16 +29,7 @@ export const Home = () => {
       <FadeIn className="home-container">
         <h1>Eduardo Bezerra</h1>
         <p>
-          <Typed
-            strings={[
-              "I'm a Full Stack Developer",
-              "I Love Software Development",
-              "I Love to code",
-            ]}
-            typeSpeed={100}
-            backSpeed={40}
-            loop
-          />
+          <span ref={el}></span>
         </p>
       </FadeIn>
     </Section>
